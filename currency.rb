3519d5b68304634d66f01cef0c3fceddde9266df
emmakcompton <emmakcompton@gmail.com>
2018-06-20 17:40:36 +0000

    Done.

--- currency.rb
+++ currency.rb
@@ -1,1 +1,20 @@
-# Write your cod here
+ def usd_to_eur 
+   euro = dollar*0.86
+   prints "euro"
+ end
+ end
+ 
+ def eur_to_usd
+   dollar = eur*1.16
+ end 
+ end
+ 
+ def usd_to_jpy
+   dollar = jpy*110.18
+  end
+end
+ 
+def usd_to_gbp
+  dollar = gbp*0.56
+end
+end
