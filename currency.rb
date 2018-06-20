3828b3df4da41a7d5249af63dcedc8734cf3ab4d
Amrita Bhasin <amritawriter7@gmail.com>
2018-06-20 17:21:15 +0000

    Done.

--- currency.rb
+++ currency.rb
@@ -1,1 +1,14 @@
 # Write your cod here
+
+def usd_to_eur(dollar)
+  euro = dollar*0.68
+  print euro
+end
+usd_to_eur
+
+def eur_to_usd(euro)
+  dollar = euro*1.16
+  print dollar
+end
+eur_to_usd
+
